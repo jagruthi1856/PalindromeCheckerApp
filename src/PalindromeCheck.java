b863680b0d52252135336ea47394b8fc3e024f68
jagruthi1856 <bj6498@srmist.edu.in>
2026-02-24 09:13:23 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheck.java
@@ -0,0 +1,7 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        System.out.printf("welcome to the Palindrome Checker Management System");
+        System.out.println("Version:1.0");
+        System.out.println("System initialized successfully");
+    }
+}
