234366a7d3adc4098266edc44cc607da77f7e988
jagruthi1856 <bj6498@srmist.edu.in>
2026-02-24 09:42:23 +0530

    UC1: Add welcome message module

--- src/PalindromeCheck.java
+++ src/PalindromeCheck.java
@@ -1,17 +1,15 @@
 public class PalindromeCheck {
     public static void main(String[] args) {
-                System.out.println("Program Started...");
-                String word = "madam";
+                String original = "level";
                 String reversed = "";
-                for (int i = word.length() - 1; i >= 0; i--) {
-                    reversed = reversed + word.charAt(i);
+                for (int i = original.length() - 1; i >= 0; i--) {
+                    reversed = reversed + original.charAt(i);
                 }
-                if (word.equals(reversed)) {
-                    System.out.println(word + " is a Palindrome.");
+                if (original.equals(reversed)) {
+                    System.out.println(original + " is a Palindrome.");
                 } else {
-                    System.out.println(word + " is NOT a Palindrome.");
+                    System.out.println(original + " is NOT a Palindrome.");
                 }
-
                 System.out.println("Program Ended.");
             }
         }
