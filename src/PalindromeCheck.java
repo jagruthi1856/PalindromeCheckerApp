7012965a2124a0dceb9504d539c71d593acd8f9e
jagruthi1856 <bj6498@srmist.edu.in>
2026-03-11 10:17:26 +0530

    Base application setup

--- src/PalindromeCheck.java
+++ src/PalindromeCheck.java
@@ -1,15 +1,28 @@
+import java.util.Stack;
+import java.util.Scanner;
 public class PalindromeCheck {
     public static void main(String[] args) {
-                String original = "level";
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter a string: ");
+                String input = sc.nextLine();
+                String str = input.toLowerCase();
+                Stack<Character> stack = new Stack<>();
+
+                for (int i = 0; i < str.length(); i++) {
+                    stack.push(str.charAt(i));
+                }
                 String reversed = "";
-                for (int i = original.length() - 1; i >= 0; i--) {
-                    reversed = reversed + original.charAt(i);
+                while (!stack.isEmpty()) {
+                    reversed = reversed + stack.pop();
                 }
-                if (original.equals(reversed)) {
-                    System.out.println(original + " is a Palindrome.");
+
+                if (str.equals(reversed)) {
+                    System.out.println("The given string is a Palindrome.");
                 } else {
-                    System.out.println(original + " is NOT a Palindrome.");
+                    System.out.println("The given string is NOT a Palindrome.");
                 }
-                System.out.println("Program Ended.");
+
+                sc.close();
             }
         }
+
